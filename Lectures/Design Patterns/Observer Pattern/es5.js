62fb63f02e2a873cf6bd83fd958ff66d9134d077
Valdemar26 <z_valdemar@ukr.net>
2018-01-25 14:17:58 +0200

    add ES6 code to Observer Pattern

--- Lectures/Design Patterns/Observer Pattern/es5.js
+++ Lectures/Design Patterns/Observer Pattern/es5.js
@@ -13,6 +13,10 @@ EventObserver.prototype = {
         console.log(`You are now subscribed to ${fn.name}`);
     },
     unsubscribe: function(fn) {
+        /* Filter out fromtheist whatever matches the callback function.
+        *  If there is no match, the callback gets to stay on the list.
+        *  The filter returns a new list and reassigns the list of observers.
+        * */
         this.observers = this.observers.filter(function(item) {
             if(item !== fn) {
                 return item;
@@ -25,7 +29,7 @@ EventObserver.prototype = {
             item.call();
         });
     }
-}
+};
 
 const click = new EventObserver();
 
@@ -46,24 +50,3 @@ document.querySelector('.fire').addEventListener('click', function() {
 const getCurMilliseconds = function() {
     console.log(`Current Milliseconds: ${new Date().getMilliseconds()}`)
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
